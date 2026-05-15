907e817428d9ac19d0e2ecaf61152b8648990b10
okbalogs <okbalogs@gmail.com>
2026-05-15 20:22:39 +0100

    buttom nav

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,9 @@
-const STATIC_CACHE = 'static-v1'
-const DYNAMIC_CACHE = 'dynamic-v1'
-const VERSE_CACHE = 'verse-v1'
+const STATIC_CACHE = 'static-v2'
+const DYNAMIC_CACHE = 'dynamic-v2'
+const VERSE_CACHE = 'verse-v2'
 
 const APP_SHELL = [
   '/',
-  '/today',
-  '/history',
-  '/settings',
-  '/entry/new',
   '/signin',
   '/signup',
 ]
