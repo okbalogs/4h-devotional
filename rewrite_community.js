9908410579d05ef9b3fc6a8d976c9144702575c0
okbalogs <okbalogs@gmail.com>
2026-07-23 20:17:16 +0100

    chore: clean up unused scripts, templates, and instruction files

--- rewrite_community.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-
-// We will extract PrayerTab, GroupsTab, PartnersTab from /tmp/old_community.jsx
-// and rewrite them manually. Wait, I can't write a script to rewrite them automatically.
